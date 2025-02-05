53b17ab29ee968cfd4fe9fee6e16e257cc15ce05
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-02-06 02:09:47 +0530

    frontend

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ChatEngine } from "react-chat-engine";
+
+import './App.css';
+
+const App=()=>{
+
+    return(
+
+        <ChatEngine
+        height="100vh"
+        projectID=""
+        userName=""
+        usersecret=""
+        />
+    );
+}
+
+export default App;
